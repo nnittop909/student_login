d7c770afebf3d6e8f2cf202fa31f18af4d8b8934
Christopher Pumihic <tophie.pumihic@gmail.com>
2017-02-23 00:20:42 +0800

    suicide notice

--- app/controllers/student_logins_controller.rb
+++ app/controllers/student_logins_controller.rb
@@ -4,9 +4,9 @@ class StudentLoginsController < ApplicationController
     if params[:id_number].present?
       @student = Student.friendly.find_by(id_number: params[:id_number])
       if @student.blank?
-        redirect_to student_logins_path, notice: 'Wrong ID Number, try again!'
+        redirect_to student_logins_path, notice: 'ID Number is incorrect or does not exist, please try again!'
       elsif @student.logins.any?
-        redirect_to student_logins_path, notice: 'You are already logged in.'
+        redirect_to student_logins_path, notice: 'You are already signed in.'
       elsif @student.present?
         @login = @student.logins.create!(:student_id => @student)
         if @login.save
